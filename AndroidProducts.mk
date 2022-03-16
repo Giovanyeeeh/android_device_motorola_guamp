808c568e7dcc56cbb3347910c31dc7a02307025a
Giovanyeeeh <86035674+Giovanyeeeh@users.noreply.github.com>
2022-03-15 21:59:38 -0300

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -17,9 +17,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-     $(LOCAL_DIR)/twrp_guamp.mk
+     $(LOCAL_DIR)/twrp_caprip.mk
 
 COMMON_LUNCH_CHOICES := \
-    twrp_guamp-user \
-    twrp_guamp-userdebug \
-    twrp_guamp-eng
+    twrp_caprip-user \
+    twrp_caprip-userdebug \
+    twrp_caprip-eng
